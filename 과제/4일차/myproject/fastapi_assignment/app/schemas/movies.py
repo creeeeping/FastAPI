498fcbcc9132a0d99eb499195746419ea0d4eb8d
admin <admin@adminui-MacBookPro.local>
2025-11-21 18:04:33 +0900

    4일차 과제

---
+++ 과제/4일차/myproject/fastapi_assignment/app/schemas/movies.py
@@ -0,0 +1,37 @@
+from typing import Any, Annotated
+
+from pydantic import BaseModel, Field
+
+from app.models.movies import GenreEnum
+
+
+class CreateMovieRequest(BaseModel):
+    title: str
+    plot: str
+    cast: dict[str, Any]
+    playtime: int
+    genre: GenreEnum
+
+
+class MovieResponse(BaseModel):
+    id: int
+    title: str
+    playtime: int
+    plot: str
+    cast: dict[str, Any]
+    genre: GenreEnum
+
+
+class MovieSearchParams(BaseModel):
+    title: str | None = None
+    genre: GenreEnum | None = None
+    plot: str | None = None
+    cast: dict[str, Any] | None = None
+
+
+class MovieUpdateRequest(BaseModel):
+    title: str | None = None
+    genre: GenreEnum | None = None
+    plot: str | None = None
+    cast: dict[str, Any] | None = None
+    playtime: Annotated[int, Field(gt=0)] | None = None
